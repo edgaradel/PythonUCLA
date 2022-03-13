0fcce1a8693662ee74e183bcf64a09171216c15b
edgaradel <edgaradel@gmail.com>
2022-03-13 12:06:18 -0600

    Create Palindrome.py

---
+++ Quality/Palindrome.py
@@ -0,0 +1,10 @@
+def is_palindrome(s):
+  """(str) -> bool
+  Return True if and only if s is a palindrome
+  >>> is_palindrome('noon')
+  True
+  >>> is_palindrome('racecar')
+  True
+  >>> is_palindrome('dented')
+  False
+  """
